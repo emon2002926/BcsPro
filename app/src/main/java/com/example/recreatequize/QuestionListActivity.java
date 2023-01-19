73c86ecdf9805d1bda409378fdd8dcda4051859c
emon <emon79605@gmail.com>
2023-01-19 16:19:51 +0600

    optimize myadapter.java and passing tha questionList[] to result activity

--- app/src/main/java/com/example/recreatequize/QuestionListActivity.java
+++ app/src/main/java/com/example/recreatequize/QuestionListActivity.java
@@ -1,15 +1,23 @@
 package com.example.recreatequize;
 
+import com.example.recreatequize.modelClass.QuestionList;
 import com.example.recreatequize.modelClass.model;
 import com.example.recreatequize.adapter.myadapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -20,12 +28,19 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionListActivity extends AppCompatActivity {
 
-    private  static final String url="http://bcs.searchwizy.com/allQuestion.php";
+    private  static final String url="http://192.168.0.103/api2/allQuestion.php";
     RecyclerView recview;
 
     private static  String url2;
+    TextView textView;
+
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,20 +50,57 @@ public class QuestionListActivity extends AppCompatActivity {
         recview=(RecyclerView)findViewById(R.id.recview);
 
         processdata();
+        url2 = getIntent().getExtras().getString("allQuestion1");
+
+        textView = findViewById(R.id.topTv);
+        textView.setText(url2);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("my_list_action"));
+
 
     }
+
+    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+
+//            String qty = intent.getStringExtra("quantity");
+
+            if (intent.getAction().equals("my_list_action")) {
+                ArrayList<QuestionList> questionLists = (ArrayList<QuestionList>) intent.getSerializableExtra("my_list_key");
+                // Do something with the list here
+
+                btnSubmit.setOnClickListener(view -> {
+
+                    Intent intent1 = new Intent(QuestionListActivity.this,QuizResult.class);
+                    //Creating Bundle To pass QuestionList
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("qutions",(Serializable) questionLists);
+
+                    intent1.putExtras(bundle);
+                    startActivity(intent1);
+                });
+
+            }
+
+        }
+    };
+
+
 
 
     public void processdata()
     {
 
-        url2 = getIntent().getExtras().getString("allQuestion");
-
 
         // Todo got the api url
-        Log.d("url2",url2);
 
-        StringRequest request=new StringRequest(url2, new Response.Listener<String>() {
+
+        StringRequest request=new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
 
