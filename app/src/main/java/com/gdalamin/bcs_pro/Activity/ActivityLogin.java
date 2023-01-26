19dd39bd82f398111ffc8a1d10c14eca1e4ed40b
emon <emon79605@gmail.com>
2023-01-26 22:33:19 +0600

    inisial google sing in added

--- app/src/main/java/com/gdalamin/bcs_pro/Activity/ActivityLogin.java
+++ app/src/main/java/com/gdalamin/bcs_pro/Activity/ActivityLogin.java
@@ -1,11 +1,15 @@
 package com.gdalamin.bcs_pro.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -17,18 +21,56 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class ActivityLogin extends AppCompatActivity {
 
 
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
-    Button googleBtn;
+    Button googleBtn,btnGenOtp,btnVerify;
+    EditText phone,otp;
+    FirebaseAuth auth;
+    String verificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phone = findViewById(R.id.numberEt);
+        otp = findViewById(R.id.otpEt);
+        btnGenOtp = findViewById(R.id.submitBtn);
+        btnVerify = findViewById(R.id.chackOtp);
+
+        auth = FirebaseAuth.getInstance();
+
+
+        btnGenOtp.setOnClickListener(view -> {
+
+            if (TextUtils.isEmpty(phone.getText().toString())){
+                Toast.makeText(ActivityLogin.this,"plese Inter a number",Toast.LENGTH_SHORT).show();
+            }else {
+                String number = phone.getText().toString().trim();
+                sendVerificationCode(number);
+            }
+
+
+        });
+        btnVerify.setOnClickListener(view -> {
+            verifyCode();
+        });
+
+
+        
 
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
@@ -39,8 +81,6 @@ public class ActivityLogin extends AppCompatActivity {
 //            navigateToSecondActivity();
 //        }
         googleBtn = findViewById(R.id.singInBtn);
-
-
         googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +90,63 @@ public class ActivityLogin extends AppCompatActivity {
 
 
     }
+
+
+
+    private void sendVerificationCode(String phoneNumber) {
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber("+880"+phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential credential)
+        {
+
+            final String code = credential.getSmsCode();
+            if (code !=null){
+                verifyCode(code);
+            }
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+
+            Toast.makeText(ActivityLogin.this,"Verification Failled",Toast.LENGTH_SHORT).show();
+
+            // Show a message and update the UI
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s,
+                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+
+            super.onCodeSent(s,token);
+            verificationId =s;
+
+        }
+    };
+    private void verifyCode(String Code) {
+
+    }
+
+
+
+
+
     void signIn(){
         Intent signInIntent = gsc.getSignInIntent();
         startActivityForResult(signInIntent,1000);
