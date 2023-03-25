b87d85afe72938ca289909c40ce1d47abf4226aa
emon <emon79605@gmail.com>
2023-03-25 23:18:36 +0600

    Ui improvement2

--- app/src/main/java/com/gdalamin/bcs_pro/Activity/QuizResult.java
+++ app/src/main/java/com/gdalamin/bcs_pro/Activity/QuizResult.java
@@ -1,28 +1,17 @@
 package com.gdalamin.bcs_pro.Activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.gdalamin.bcs_pro.R;
 import com.gdalamin.bcs_pro.modelClass.QuestionList;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class QuizResult extends AppCompatActivity {
 
@@ -40,22 +29,17 @@ public class QuizResult extends AppCompatActivity {
         final TextView totalScoreTv = findViewById(R.id.totalScoreTv);
         final TextView correctTv = findViewById(R.id.correctTv);
         final TextView incorrect = findViewById(R.id.incorrectTv);
-//        final AppCompatButton shareBtn = findViewById(R.id.shareBtn);
         final AppCompatButton reTakeBtn = findViewById(R.id.reTakeQuizBtn);
-        final TextView textViewCongras = findViewById(R.id.congratulation);
-        final TextView textViewgerting = findViewById(R.id.textViewgereting);
+
         reTakeBtn.setOnClickListener(view -> {
             finish();
-            onBackPressed();
+            startActivity(new Intent(QuizResult.this,MainActivity.class));
 
         });
 
 
         questionLists = (List<QuestionList>) getIntent().getSerializableExtra("qutions");
         totalScoreTv.setText("/"+"10");
-
-        String answerd = getIntent().getStringExtra("answerd");
-        scoreTv.setText(String.valueOf(getCorrectAnswer()+""));
 
 
         correctTv.setText(getCorrectAnswer()+"");
@@ -88,22 +72,6 @@ public class QuizResult extends AppCompatActivity {
         }
         return correctAnswer;
     }
-
-
-/*
-private  int answeredQuestion (){
-
-    for (int i = 0; i < questionLists.size(); i++) {
-        if (questionLists.get(i).getUserSelecedAnswer() != 0) {
-            answeredQuestions++;
-
-        }
-    }
-    return answeredQuestions;
-
-}
-
- */
 
 
 
